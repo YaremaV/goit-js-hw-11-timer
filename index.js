4510908b6f926d720e8fbb9ad67da1fcc4e7ba2b
YaremaV <Yaremenko1996v@gmail.com>
2021-08-06 23:04:15 +0300

    Добавил код для решения обратного отсчёта таймера

--- index.js
+++ index.js
@@ -3,39 +3,58 @@ const refs = {
     days: document.querySelector('span[data-value="days"]'),
     hours: document.querySelector('span[data-value="hours"]'),
     mins: document.querySelector('span[data-value="mins"]'),
-    secs: document.querySelector('span[data-value="secs"]')
+    secs: document.querySelector('span[data-value="secs"]'),
+    timer: document.getElementById('#timer-1')
 }
-
-
 class CountdownTimer {
-  constructor({ onTick }) {
+  constructor({ targetDate, selector }) {
     this.intervalId = null;
-    this.isActive = false;
-    this.onTick = onTick;
+    this.targetDate = targetDate;
+    this.selector = selector;
+    this.onTick = this.updateClockFace;
 
-    // this.init();
+    this.start()
+
+    
   }
 
-    getTimeComponents(time) {
+   start() {
+    this.intervalId = setInterval(() => {
+      const targetDate = this.targetDate.getTime();
+      const currentTime = Date.now();
+      const deltaTime = targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+
+  getTimeComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
     return {days, hours, mins, secs };
   }
 
-  /*
-   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
-   */
   pad(value) {
     return String(value).padStart(2, '0');
   }
+
+ updateClockFace(time = {days, hours, mins, secs }) {
+  const timerEl = document.querySelector(this.selector);
+    for (const key in time) {
+      const valueEl = timerEl.querySelector(`[data-value="${key}"]`);
+      valueEl.innerHTML = `<span>${time[key]}</span>`;
+    }
+  
+}
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Jul 27, 2022'),
 });
+
+
